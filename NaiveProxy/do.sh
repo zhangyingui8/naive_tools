2975601c5a9f4640937b74310d2a842a1cc4dd9b
zhangyingui8 <71049652+zhangyingui8@users.noreply.github.com>
2025-09-02 09:00:50 +0800

    更新 do.sh
    //update link target into my account

--- NaiveProxy/do.sh
+++ NaiveProxy/do.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 shell_renew(){
-    curl -o /root/.naive.sh https://raw.githubusercontent.com/imajeason/nas_tools/main/NaiveProxy/naive.sh 
+    #curl -o /root/.naive.sh https://raw.githubusercontent.com/imajeason/nas_tools/main/NaiveProxy/naive.sh 
+    curl -o /root/.naive.sh https://raw.githubusercontent.com/zhangyingui8/naive_tools/main/NaiveProxy/naive.sh
     chmod +x /root/.naive.sh
     ln -s /root/.naive.sh /usr/bin/naive
     echo
