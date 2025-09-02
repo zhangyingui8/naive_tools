7a6af0103e5f42621e3d1350ccaab8f012b490c1
zhangyingui8 <71049652+zhangyingui8@users.noreply.github.com>
2025-09-02 09:30:46 +0800

    更新 do.sh
    //update new file name

--- NaiveProxy/do.sh
+++ NaiveProxy/do.sh
@@ -2,9 +2,10 @@
 
 shell_renew(){
     #curl -o /root/.naive.sh https://raw.githubusercontent.com/imajeason/nas_tools/main/NaiveProxy/naive.sh 
-    curl -o /root/.naive.sh https://raw.githubusercontent.com/zhangyingui8/naive_tools/main/NaiveProxy/naive.sh
-    chmod +x /root/.naive.sh
-    ln -s /root/.naive.sh /usr/bin/naive
+    #curl -o /root/.naive.sh https://raw.githubusercontent.com/zhangyingui8/naive_tools/main/NaiveProxy/naive.sh
+    curl -o /root/.naive.sh https://raw.githubusercontent.com/zhangyingui8/naive_tools/main/NaiveProxy/naive copy.sh
+    chmod +x /root/.naive copy.sh
+    ln -s /root/.naive copy.sh /usr/bin/naive
     echo
     echo " naive 命令安装完毕，请使用naive进行操作。"
 }
