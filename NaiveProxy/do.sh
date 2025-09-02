5dfaf4c01ea74876cfc2a4ad2dd08e3704029f56
zhangyingui8 <71049652+zhangyingui8@users.noreply.github.com>
2025-09-02 09:33:07 +0800

    更新 do.sh

--- NaiveProxy/do.sh
+++ NaiveProxy/do.sh
@@ -3,7 +3,7 @@
 shell_renew(){
     #curl -o /root/.naive.sh https://raw.githubusercontent.com/imajeason/nas_tools/main/NaiveProxy/naive.sh 
     #curl -o /root/.naive.sh https://raw.githubusercontent.com/zhangyingui8/naive_tools/main/NaiveProxy/naive.sh
-    curl -o /root/.naive.sh https://raw.githubusercontent.com/zhangyingui8/naive_tools/main/NaiveProxy/naive copy.sh
+    curl -o /root/.naive copy.sh https://raw.githubusercontent.com/zhangyingui8/naive_tools/main/NaiveProxy/naive copy.sh
     chmod +x /root/.naive copy.sh
     ln -s /root/.naive copy.sh /usr/bin/naive
     echo
