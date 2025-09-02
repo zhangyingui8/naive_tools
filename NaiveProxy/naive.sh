7f63f4e2f30de448cc01e00e323af13affe50136
zhangyingui8 <71049652+zhangyingui8@users.noreply.github.com>
2025-09-02 08:56:42 +0800

    更新 naive.sh
    //Update keyword to download from tag.

--- NaiveProxy/naive.sh
+++ NaiveProxy/naive.sh
@@ -259,7 +259,7 @@ install_caddy() {
     cd /root/src/
     rm caddy-forwardproxy-naive.tar.xz
     #wget https://github.com/klzgrad/forwardproxy/releases/download/v2.7.5-caddy2-naive2/caddy-forwardproxy-naive.tar.xz
-    wget https://github.com/klzgrad/forwardproxy/releases/tag/v2.10.0-naive/caddy-forwardproxy-naive.tar.xz
+    wget https://github.com/klzgrad/forwardproxy/releases/download/v2.10.0-naive/caddy-forwardproxy-naive.tar.xz
     tar xvf caddy-forwardproxy-naive.tar.xz 
     systemctl stop naive
     \cp caddy-forwardproxy-naive/caddy /usr/bin/
